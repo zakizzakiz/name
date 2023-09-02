6b8c743bc9055bc1a0844abc228c701c5d6acc7c
zakiz <khishigee0105gmail.com>
2023-09-02 12:40:37 +0800

    toonii mashin oruullaa

---
+++ lab3.js
@@ -0,0 +1,83 @@
+// var name =  " monhhishig";
+//console.log("hi"+name)
+// var age=+prompt("nas");
+// if(age>18){
+//     alert("nasand hursen");
+// }else if(age<18 && age>=0){
+//     alert("nylh huuhed baina")
+// }
+// else{
+//     alert("nasand hureegu hun bna")
+// }
+
+
+
+// var too=+prompt("too");
+// if(too%2==0){
+//     alert("tegesh too baina")
+// }else{
+// alert("sondgoi too baina")
+// }
+
+
+
+// var number2 = +prompt("too oruulna uu");
+// if(number2<0){
+//     alert("surug too")
+// }else{
+//     alert("eyreg too");
+// 
+
+
+// var today = 6;
+// switch(today){
+//   case 3:
+//     console.log("4th day")
+//     break;
+//   case 4:
+//     console.log("saturday")
+//     break;
+//     case 5:
+//         console.log("friday")
+//         break;
+//       case 6:
+//         console.log("2th day")
+//         break;
+//         case 2:
+//             console.log("2th day")
+//             break;
+//           case 1:
+//             console.log("saturday")
+//             break;
+//   default:
+//   console.log("not found")
+//   break;
+// }
+
+
+var too1 =+prompt("too")
+var op = prompt("temdeg")
+var too2 =+prompt("too2")
+switch(op){
+    case "+":
+    alert(too1 + too2);
+    break;
+    case "-":
+        alert(too1-too2);
+        break;
+        case "x":
+            alert(too1*too2);
+            break;
+        case ".":
+         alert(too1/too2);
+        break;
+        
+        default:
+               console.log("not found")
+              break;
+
+}
+
+
+
+
